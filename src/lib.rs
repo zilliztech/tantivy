5428d6413ed8ef6f3d0c87a3d182b4d49d42cf3b
Buqian Zheng <zhengbuqian@gmail.com>
2026-03-30 17:17:15 +0800

    fix: rustfmt split grouped pub use into separate lines
    Signed-off-by: Buqian Zheng <zhengbuqian@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -164,8 +164,10 @@ mod macros;
 mod future_result;
 
 // Re-exports
+pub use columnar;
 pub use common::DateTime;
-pub use {columnar, query_grammar, time};
+pub use query_grammar;
+pub use time;
 
 pub use crate::error::TantivyError;
 pub use crate::future_result::FutureResult;
